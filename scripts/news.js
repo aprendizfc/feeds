beeb164ed724fd164f34d65dd36a5b219bdf0bf2
aprendizfc <richard11_71@outlook.com>
2016-03-17 11:47:08 -0500

    make an http request to fill out the news array

---
+++ scripts/news.js
@@ -0,0 +1,9 @@
+angular.module('feedApp')
+	.controller('NewsCtrl', ['$rootScope', '$scope', '$http' function($rootScope, $scope, $http) {
+		$scope.news = [];
+		$http.get($rootScope.endpoint).then(function(response) {
+			$scope.news = response.data;
+		}, function(error) {
+			console.log(error);
+		});
+	}])
