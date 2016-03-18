cbd3eb5e6636c3462471371241bb4d1774544989
Ricardo <richard11_71@outlook.com>
2016-03-17 21:57:08 -0500

    create function to get news

--- scripts/news.js
+++ scripts/news.js
@@ -1,9 +1,11 @@
 angular.module('feedApp')
 	.controller('NewsCtrl', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
 		$scope.news = [];
-		$http.get($rootScope.endpoint).then(function(response) {
-			$scope.news = response.data;
-		}, function(error) {
-			console.log(error);
-		});
+		$scope.getNews = function() {
+			$http.get($rootScope.endpoint).then(function(response) {
+				$scope.news = response.data;
+			}, function(error) {
+				console.log(error);
+			});
+		}
 	}])
