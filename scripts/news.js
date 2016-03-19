8cd7931768ef6defc69e1906e9859ca842283094
Ricardo <richard11_71@outlook.com>
2016-03-19 18:24:41 -0500

    display the news' title in the header

--- scripts/news.js
+++ scripts/news.js
@@ -1,5 +1,5 @@
 angular.module('feedApp')
-	.controller('NewsCtrl', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
+	.controller('NewsCtrl', ['$rootScope', '$scope', '$http', '$timeout', function($rootScope, $scope, $http, $timeout) {
 		$scope.news = [];
 		$scope.getNews = function() {
 			$http.get($rootScope.endpoint).then(function(response) {
@@ -11,10 +11,13 @@ angular.module('feedApp')
 
 		$scope.showNews = function(id) {
 			var $cardContent = $("#" + id);
+			var $title = $('.' + id).text();
 			if ( $cardContent.hasClass('active') ) {
+				$("#title").text($title).removeClass('bounceInDown').addClass('bounceOutLeft')
 				$cardContent.removeClass('active');
 			} else {
 				$cardContent.addClass('active');
+				$("#title").text($title).removeClass('bounceOutLeft').addClass('animated bounceInDown');
 			}
 		}
 	}])
