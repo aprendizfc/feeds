eb9078c823059b0b36a2c08979ad245ccf57a4b5
Ricardo <richard11_71@outlook.com>
2016-03-19 22:20:22 -0500

    reset the title inside the header while getting news

--- scripts/news.js
+++ scripts/news.js
@@ -2,6 +2,7 @@ angular.module('feedApp')
 	.controller('NewsCtrl', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
 		$scope.news = [];
 		$scope.getNews = function() {
+			$("#title").removeClass('bounceInDown').html('');
 			$http.get($rootScope.endpoint).then(function(response) {
 				$scope.news = response.data;
 			}, function(error) {
